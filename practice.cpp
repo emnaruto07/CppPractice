57791728955bcb7f162905da0d88a057967c9861
emnaruto07 <shazebvps@gmail.com>
2021-05-09 04:57:38 +0530

    stdlib system

--- practice.cpp
+++ practice.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 
 int main(){
 cout << "hello World!\n";
+
+system("rm practice");
+cout << "[+]Practice Removed!!\n";
+system("ls -la");
 return 0;
 
 }
