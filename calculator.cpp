8fd84cc6023b3d92edbf590d928d0e01edaee9a4
emnaruto07 <shazebvps@gmail.com>
2021-05-23 15:07:18 +0530

    Added all functions

--- calculator.cpp
+++ calculator.cpp
@@ -7,6 +7,21 @@ int Add(int x, int y){
    return c;
 }
 
+int Sub(int x, int y){
+    int z = x - y;
+    return z;
+}
+
+int Multiple(int x, int y){
+    int z = x * y;
+    return z;
+}
+
+int Divide(int x, int y){
+    int z = x / y;
+    return z;
+}
+
 int main(){
     int a;
     cout << "Enter the first value: ";
@@ -16,7 +31,13 @@ int main(){
     cin >> b;
 
     int addition = Add(a,b);
+    int substraction = Sub(a,b);
+    int multiple = Multiple(a,b);
+    int divide = Divide(a,b);
     cout << "Your Addition value is: "<< addition << "\n";
+    cout << "Your Substraction value is: "<< substraction << "\n";
+    cout << "Your Addition value is: "<< multiple << "\n";
+    cout << "Your Addition value is: "<< divide << "\n";
 }
 
 
---
+++ compile.sh
@@ -0,0 +1,4 @@
+echo "removing binary.."
+rm calculator
+echo "compiling binary.."
+g++ $1 -o calculator
